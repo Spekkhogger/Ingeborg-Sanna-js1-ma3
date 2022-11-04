69c2571d31aa7e49897367469c56111537f6a4bd
Ingeburgern <56824264+Spekkhogger@users.noreply.github.com>
2022-11-04 12:19:32 +0100

    Update question-2

--- Scripts/question-2.js
+++ Scripts/question-2.js
@@ -1,9 +1,31 @@
 //Question 2
 
-const key = a8303e18e1cd42079c0470cda94f5cbe
-const url = `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=${key}`
+const key = "a8303e18e1cd42079c0470cda94f5cbe";
+const url = `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=${key}`;
+
+const content = document.querySelector(".content"); 
 
 const getData = async() => {
     const response = await fetch(url); 
     const data = await response.json();
+    const resultData = data.results;
+
+    console.log(resultData); 
+
+    for (let i = 0; i < 8; i++){
+        const gameName = resultData[i].name;
+        const gameRating = resultData[i].rating;
+        const numberOfTags = resultData[i].tags.length;
+        content.innerHTML += `
+        Name: ${gameName}
+        Rating: ${gameRating}
+        Number of tags: ${gameRating}
+        `;
+    }
 }
+
+getData(); 
+
+
+//Loading display...
+//Handle any potential errors in the code (try / catch)
