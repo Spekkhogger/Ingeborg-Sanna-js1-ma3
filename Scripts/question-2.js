222bc981ad495ed40468a01c362514879168d259
Ingeburgern <56824264+Spekkhogger@users.noreply.github.com>
2022-11-06 14:51:12 +0100

    add errorhandling

--- Scripts/question-2.js
+++ Scripts/question-2.js
@@ -6,35 +6,34 @@ const url = `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=
 const content = document.querySelector(".content"); 
 
 const getData = async() => {
-    const response = await fetch(url); 
-    const data = await response.json();
-    const resultData = data.results;
+    try {
+        const response = await fetch(url); 
+        const data = await response.json();
+        const resultData = data.results;
 
-    content.innerHTML = "";
+        content.innerHTML = "";
 
-    for (let i = 0; i < resultData.length; i++){
-        if (i === 8) {
-            break;
-        }
-        const gameName = resultData[i].name;
-        const gameRating = resultData[i].rating;
-        const numberOfTags = resultData[i].tags.length;
-        content.innerHTML += `<ul>
-        <li> Name: ${gameName} </li>
-        <li> Rating: ${gameRating} </li>
-        <li> Number of tags: ${numberOfTags} </li>
-        </ul>
-        `;
+        for (let i = 0; i < resultData.length; i++){
+            if (i === 8) {
+                break;
+            }
+            const gameName = resultData[i].name;
+            const gameRating = resultData[i].rating;
+            const numberOfTags = resultData[i].tags.length;
+            content.innerHTML += `<ul>
+            <li> Name: ${gameName} </li>
+            <li> Rating: ${gameRating} </li>
+            <li> Number of tags: ${numberOfTags} </li>
+            </ul>
+            `;
+    }
+    } catch(error){
+        console.log("Error occurred: " + error);
+        content.innerHTML = errorMessage("Something went wrong...")
     }
 }
 
-try {
     getData();
-} catch (error){
-    console.log("Something went wrong: " + error);
-    content.innerHTML = "Something went wrong... try reloading the page"
-}
-
 
 
 //Loading display...
